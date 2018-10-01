28b63a4b9b6c98fa7d527efbc31251471dfb77cb
Nightsroud <nescudero57@gmail.com>
2018-10-01 20:05:31 -0300

    Clase 02

---
+++ Perceptron/Percept/Perceptrons.py
@@ -0,0 +1,42 @@
+class Perceptron(object):
+    def __init__(self, weights, bias):
+        self.weight = weights
+        self.bias = bias
+
+    def feed(self, inputs):
+        assert inputs.length == self.weight.length
+        r = 0
+        for x in range(inputs.length):
+            r += self.weight[x]*inputs[x]
+        r+= self.bias
+        return r > 0
+
+class pAND(Perceptron):
+
+    def __init__(self):
+        Perceptron.__init__(self, [1.0, 1.0], -1.5)
+
+class pOR(Perceptron):
+
+    def __init__(self):
+        Perceptron.__init__(self, [1.0, 1.0], -0.5)
+
+class pNAND(Perceptron):
+
+    def __init__(self):
+        Perceptron.__init__(self, [-2.0, -2.0], 3)
+
+class pSum(object):
+    sNAND = pNAND
+    def __init__(self, x1, x2):
+        self.x1 = x1
+        self.x2 = x2
+
+    def feed(self):
+        result1 = self.sNAND.feed([self.x1, self.x2])
+        carry = self.sNAND.feed(result1, result1)
+        result2 = self.sNAND.feed([self.x1, result1])
+        result3 = self.sNAND.feed([result1, self.x2])
+        result = self.sNAND.feed([result2, result3])
+        return (result, carry)
+
